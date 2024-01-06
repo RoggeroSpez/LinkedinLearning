a8397699bf917f103e1fc80539e2332a7aefac92
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2024-01-06 12:33:25 -0800

    ESLint

--- Ex_Files_JavaScript_Best_Practices_2023/Ex_Files_JavaScript_Best_Practices_2023/Exercise Files/Ch02/02_01/begin/app.js
+++ Ex_Files_JavaScript_Best_Practices_2023/Ex_Files_JavaScript_Best_Practices_2023/Exercise Files/Ch02/02_01/begin/app.js
@@ -2,9 +2,17 @@
 
 const today = new Date();
 
+function weekAgoDate() {
+  const date = new Date();
+  date.setDate(date.getDate() - 7);
+  return date;
+}
   // const date = new Date();
   // date.setDate(date.getDate() - 7);
   // return date;
+
+const weekAgo = weekAgoDate();
+
 
 console.log(today);
 console.log(weekAgo);
